7b10fd8463f1264789a9ee462881a0dce1b7364f
kwsantiago <kwsantiago@mail.usf.edu>
2020-06-14 21:10:08 -0400

    Add 365

--- freeFood.c
+++ freeFood.c
@@ -6,11 +6,14 @@ int main(int argc, char* argv[]){
     for(i = 0; i < numLines; i++){
         scanf("%d %d", &num1, &num2);
         tmp = num2 - num1;
-        printf("\n%d\n", tmp);
         if(tmp == 0)
             total++;
         else
-            total += tmp+1;
+            total += ++tmp;
+    }
+    if(total >= 365){
+        printf("365\n");
+        return 0;
     }
     printf("%d\n", total);
     return 0;
