afd4a07625958f577ffd0f33683a8d84acb6f2ee
Kyle <kwsantiago@usf.edu>
2022-06-20 11:20:20 -0400

    Simplify Code and save memory

--- Eye of Sauron/src/main.rs
+++ Eye of Sauron/src/main.rs
@@ -3,16 +3,14 @@ use std::io;
 fn main() {
     let mut vert_current: u64 = 0;
     let mut vert_seen: u64 = 0;
-    let vert: char = '|';
-    let closing: char = ')';
     let mut string = String::new();
 
     io::stdin().read_line(&mut string).unwrap();
 
     for i in string.chars() {
-        if i == vert {
+        if i == '|' {
             vert_current += 1;
-        } else if i == closing {
+        } else if i == ')' {
             vert_seen = vert_current;
             vert_current = 0; // Start counting verts again
         }
